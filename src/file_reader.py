23d622a451e3dbcd787e4839c7cb983ef702ea62
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-06-07 14:57:31 -0700

    Added make title

---
+++ src/file_reader.py
@@ -0,0 +1,14 @@
+import urllib.request
+from bs4 import BeautifulSoup
+def read_url(url):
+    with urllib.request.urlopen(url) as response:
+        return response.read().decode('utf-8')
+    
+def get_title(url):
+    html = read_url(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    title_element = soup.find('h1', class_='title mathjax')
+    if title_element:
+        return title_element.text.replace('Title:', '')
+    else:
+        return None
