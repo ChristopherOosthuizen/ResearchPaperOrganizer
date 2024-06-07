882abffb180a25abc361c419175627b3a0c875f9
ChristopherOosthuizen <christopher.jan.oosthuizen@gmail.com>
2024-06-07 14:41:56 -0700

    Added fuzzy descriptions added

--- src/AI_methods.py
+++ src/AI_methods.py
@@ -16,10 +16,18 @@ def generate_text(system_prompt, prompt):
     )
     return response.choices[0].message.content
 
-def summarization(doc):
+def summarization(doc,name):
     key = open("openaikey","r").read()
     os.environ["OPENAI_API_KEY"] = key
     documents = SimpleDirectoryReader(doc).load_data()
     index = VectorStoreIndex.from_documents(documents)
     query_engine = index.as_query_engine()
-    return query_engine.query("Create me a indepth summerization of the doucment in about 250 words.").response
+    return query_engine.query("Document name: "+name+" Create me a indepth summerization of the doucment in about 250 words.").response
+
+def generate_fuzzy_description(doc,name):
+    key = open("openaikey","r").read()
+    os.environ["OPENAI_API_KEY"] = key
+    documents = SimpleDirectoryReader(doc).load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    query_engine = index.as_query_engine()
+    return query_engine.query("Given the name and the context Make me a few search prompts that this document would be a good match for seperate your lines with only newlines name: "+name).response
